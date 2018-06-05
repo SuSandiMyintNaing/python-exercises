49e067973e0c215cbfd489ec87229bd9c0d3fe73
SuSandiMyintNaing <susandi1410@gmail.com>
2018-06-05 15:04:48 +0630

    change

---
+++ ex1.py
@@ -0,0 +1,14 @@
+print("Hello World!")
+print("Hello again")
+print("I like tying this")
+print("this is fun")
+print ('Yay! printing')
+print ("I'd must rather you 'not'.")
+print ('I "said" do not touch this')
+ 
+
+
+
+
+
+
