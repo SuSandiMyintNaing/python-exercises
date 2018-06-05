6b280ed37b8b3065fc56eee16c144e9d10b98c3e
SuSandiMyintNaing <susandi1410@gmail.com>
2018-06-05 22:44:28 +0630

    My pyton exercise.

---
+++ ex9.py
@@ -0,0 +1,13 @@
+# Here's some new strange stuff . remember type it exactly.
+days = "Mon tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("here are the days: " , days)
+print("Here are the months: ",months)
+
+print("""
+        there's something going on here.
+        with the three double-quotes.
+        We'll be able to type as much as we like.
+        Even 4 lines if we want. or 5, or 6.
+        """)
