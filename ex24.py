5c22b0b4fdbdacc6cd1db125aa9304912568e658
SuSandiMyintNaing <susandi1410@gmail.com>
2018-06-11 15:43:26 +0630

    exercise ex24.py

--- ex24.py
+++ ex24.py
@@ -1,5 +1,6 @@
-print "Let's practice everying."
-print 'You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.'
+print("Let's practice everying.")
+print('You\'d need to know \'bout escapes with \\ that do')
+print('\n newlines and \t tabs.')
 
 poem = """
 \tThe lovely world
@@ -10,61 +11,29 @@ and requires an explanation
 \n\t\twhere there is none.
 """
 
-print "---------------"
-print poem
-print "---------------"
+print ("---------------")
+print (poem)
+print ("---------------")
 
 five = 10 - 2 + 3 - 6
-print "This should be five : %s" % five
-
-def secret_formula(started):
-    jelly_beans = started * 500
-	jars = jelly_beans / 1000
-	crates = jars / 1000
-	return jelly_beans, jars, crates
-	
-start_point = 10000
-beans, jars,crates = secret_formula(start_point)
-
-print "With a starting point of: %d" % start_point
-print "We'd have %d beans, %d jars, and %d crates." % (beans , jars, crates)
-
-start_point = start_point / 10
-
-print "We can also do that this wa practice everying."
-print 'You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.'
-
-poem = """
-\tThe lovely world
-with logic so firmly planted
-cannot discern \n the needs of lovely
-nor comprehend passion from intuition
-and requires an explanation
-\n\t\twhere there is none.
-"""
-
-print "---------------"
-print poem
-print "---------------"
-
-five = 10 - 2 + 3 - 6
-print "This should be five : %s" % five
+print ("This should be five : {five}")
 
 def secret_formula(started):
     jelly_beans = started * 500
     jars = jelly_beans / 1000
     crates = jars / 1000
     return jelly_beans, jars, crates
-    
-    start_point = 10000
-    beans, jars,crates = secret_formula(start_point)
-    
-    print "With a starting point of: %d" % start_point
-    print "We'd have %d beans, %d jars, and %d crates." % (beans , jars, crates)
-    
-    start_point = start_point / 10
-    
-    print "We can also do that this way:"
-    print "We'd have %d beans, %d jars, and %d crates." %secret_formula(start_point)
-    :"
-print "We'd have %d beans, %d jars, and %d crates." %secret_formula(start_point)
+
+start_point = 10000
+beans, jars,crates = secret_formula(start_point)
+
+print ("With a starting point of: {}". format(start_point))
+print ("We'd have {beans} beans, {jars} jars, and {crates} crates." )
+
+start_point = start_point / 10
+
+print ("We can also do that this way:")
+formula = secret_formula(start_point)
+
+print ("We'd have {} beans, {} jars, and {} crates.".format(*formula))
+
