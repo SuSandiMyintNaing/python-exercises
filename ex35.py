88e02b0c7adb0d86bf92a74a5650ed93f63f89e2
SuSandiMyintNaing <susandi1410@gmail.com>
2018-06-11 17:59:08 +0630

    exercise ex35.py

--- ex35.py
+++ ex35.py
@@ -2,7 +2,7 @@
 from sys import exit
 
 def gold_room():
-        print "This room is full of gold. How much do you take?"
+        print ("This room is full of gold. How much do you take?")
 
         next = raw_input("> ")
         if "0" in next or "1" in next:
