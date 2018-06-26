86c8cdf674cec37598e2422443d8f86feed2430f
SuSandiMyintNaing <susandi1410@gmail.com>
2018-06-26 09:13:25 +0630

    Changed cat

---
+++ ex101.py
@@ -0,0 +1,15 @@
+taddy_dog = "I'm tabbed in."
+persian_dog = "I'm split\non a line."
+backslash_dog = "I'm \\ a \\ dog."
+
+fat_dog = """
+        I'll do a list:
+        \t* Dog food
+        \t*Fishies
+        \t*Catnip\n\t* Grass
+        """
+
+print(taddy_dog)
+print(persian_dog)
+print(backslash_dog)
+print(fat_dog)
