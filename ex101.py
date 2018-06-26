eaf04d7c02f330931b7db0c2a777ca4c28382da5
SuSandiMyintNaing <39304965+SuSandiMyintNaing@users.noreply.github.com>
2018-06-26 09:15:55 +0630

    changed dog to red

--- ex101.py
+++ ex101.py
@@ -1,15 +1,15 @@
-taddy_dog = "I'm tabbed in."
-persian_dog = "I'm split\non a line."
-backslash_dog = "I'm \\ a \\ dog."
+taddy_red = "I'm tabbed in."
+persian_red = "I'm split\non a line."
+backslash_red = "I'm \\ a \\ red."
 
-fat_dog = """
+fat_red = """
         I'll do a list:
         \t* Dog food
         \t*Fishies
         \t*Catnip\n\t* Grass
         """
 
-print(taddy_dog)
-print(persian_dog)
-print(backslash_dog)
-print(fat_dog)
+print(taddy_red)
+print(persian_red)
+print(backslash_red)
+print(fat_red)
