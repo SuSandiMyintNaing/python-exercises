03f12459ab45f97b0c068c68f77683763c6688f3
SuSandiMyintNaing <susandi1410@gmail.com>
2018-06-28 09:26:59 +0630

    Sitepackage for ex41

--- sitepackage/sitepackage41.py
+++ sitepackage/sitepackage41.py
@@ -7,69 +7,69 @@ WORDS = []
 
 PHRASES = {
         "class %%%(%%%):":
-        "Make a class named %%% that is-a %%%.",
-        "class %%%(object):\n\tdef __init__(self, ***)" :                      "class %%% has-a __init__ that takes self and *** parameters.",
-        "class %%%(object):\n\tdef ****(self, @@@)",                           "class %%5 has-a function named *** that takes self and @@@ pameters",                                                                                                        "*** = %%%()":
-                                                                                                                        "Set *** to an instance of class %%%.",
-                                                                                                                                    "***.***(@@@)":
-                                                                                                                                                    "From *** get the *** function, and call it with parameters self , @@@.",
-                                                                                                                                                                "***.*** = '***'":
-                                                                                                                                                                                "From *** get the *** attribute and set it to '***'."
-                                                                                                                                                                                                              
-                                                                                                                                                                                            }
+            "Make a class named %%% that is-a %%%.",
+        "class %%%(object):\n\tdef __init__(self, ***)" :                          "class %%% has-a __init__ that takes self and *** parameters.",
+        "class %%%(object):\n\tdef ****(self, @@@)",                               "class %%5 has-a function named *** that takes self and @@@ pameters",                                                                   "*** = %%%()":
+           "Set *** to an instance of class %%%.",
+       "***.***(@@@)":
+           "From *** get the *** function, and call it with parameters self , @@@.",
+       "***.*** = '***'":
+           "From *** get the *** attribute and set it to '***'."
+           
+       }
 
 #do they want to drill phrases first
 
 if len(sys.argv) == 2 and sys.argv[1] == "english":
-        PHRASE_FIRST == True 
-    else:
-            PHRASE_FIRST = False
+    PHRASE_FIRST == True 
+else:
+    PHRASE_FIRST = False
 
-            #load up the words from the website
-            for word in urlopen(WORD_URL).readlines():
-                    WORDS.append(str(word.strip(), encoding="utf-8"))
+#load up the words from the website
+for word in urlopen(WORD_URL).readlines():
+    WORDS.append(str(word.strip(), encoding="utf-8"))
 
-                    def convert(snippet, phrase):
-                            class_names = [w.capitalize() for w in random.sample(WORDS, snippet.count("%%%"))]
-                                other_names = random.sample(WORDS, snippet.count("***"))
-                                    results = []
-                                        param_names = []
+def convert(snippet, phrase):
+    class_names = [w.capitalize() for w in random.sample(WORDS, snippet.count("%%%"))]
+    other_names = random.sample(WORDS, snippet.count("***"))
+    results = []
+    param_names = []
 
-                                            for i in range(0, snippet.count("@@@")):
-                                                        param_count = random.randint(1,3)
-                                                                param_names.append(', '.join(random.sample(WORDS, param_count)))
+    for i in range(0, snippet.count("@@@")):
+        param_count = random.randint(1,3)
+        param_names.append(', '.join(random.sample(WORDS, param_count)))
                                                                     
-                                                                        for sentence in snippet, phrase:
-                                                                                    result = sentence[:]
-                                                                                            
-                                                                                                #fake class names
-                                                                                                    for word in class_names:
-                                                                                                                result = result.replace("%%%", word, 1)
-                                                                                                                        
-                                                                                                                            #fake other names
-                                                                                                                                for word in other_names:
-                                                                                                                                            result = result.replace("***", word, 1)
-
-                                                                                                                                                #fake parameter list
-                                                                                                                                                    for word in param_names:
-                                                                                                                                                                result = result.replace("@@@", word, 1)
-                                                                                                                                                                        result.append(result)
-                                                                                                                                                                                return results
-                                                                                                                                                                                
-                                                                                                                                                                                #keep going until they hit CTRL-DeprecationWarning
-                                                                                                                                                                                    try:                                                                                                           
-                                                                                                                                                                                                while True:
-                                                                                                                                                                                                                snippets = list(PHRASES.keys())
-                                                                                                                                                                                                                            random.shuffle(snippets)
-                                                                                                                                                                                                                                        
-                                                                                                                                                                                                                                                    for snippet in snippets:
-                                                                                                                                                                                                                                                                        phrase = PHRASES[snippet]
-                                                                                                                                                                                                                                                                                        question, answer = convert(snippet, phrase)
-                                                                                                                                                                                                                                                                                                        if PHRASEA_FIRST:
-                                                                                                                                                                                                                                                                                                                                question, answer = answer, question
-                                                                                                                                                                                                                                                                                                                                                    
-                                                                                                                                                                                                                                                                                                                                                                    print(question)
-                                                                                                                                                                                                                                                                                                                                                                                    input("> ")
-                                                                                                                                                                                                                                                                                                                                                                                                    print(f"ANSWER: {answer}\n\n")
-                                                                                                                                                                                    except E0FError:
-                                                                                                                                                                                            print ("\nBye")
+    for sentence in snippet, phrase:
+        result = sentence[:]                            
+        
+        #fake class names
+        for word in class_names:
+            result = result.replace("%%%", word, 1)
+            
+        #fake other names
+        for word in other_names:
+            result = result.replace("***", word, 1)
+            
+        #fake parameter list
+        for word in param_names:
+            result = result.replace("@@@", word, 1)
+            result.append(result)
+            return result
+        
+        #keep going until they hit CTRL-DeprecationWarning
+        try:
+            while True:
+                snippets = list(PHRASES.keys())
+                random.shuffle(snippets)
+                
+                for snippet in snippets:
+                    phrase = PHRASES[snippet]
+                    question, answer = convert(snippet, phrase)
+                    if PHRASEA_FIRST:
+                        question, answer = answer, question
+                        
+                    print(question)
+                    input("> ")
+                    print(f"ANSWER: {answer}\n\n")
+    except E0FError:
+        print ("\nBye")
